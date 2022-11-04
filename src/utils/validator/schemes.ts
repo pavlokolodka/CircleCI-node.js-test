45d6217491eb890ebb12896230de9cb8317b204c
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-11-04 21:36:41 +0200

    Add user update validation

--- src/utils/validator/schemes.ts
+++ src/utils/validator/schemes.ts
@@ -1,2 +1,3 @@
 export { CreateUserSchema } from './user/createUser.schema';
 export { LoginUserSchema } from './user/loginUser.schema';
+export { UpdateUserSchema } from './user/updateUser.schema';
--- src/utils/validator/types.ts
+++ src/utils/validator/types.ts
@@ -1,4 +1,5 @@
 import { CreateUserDto } from "src/auth/dto/create-user.dto";
 import { LoginUserDto } from "src/auth/dto/login-user.dto";
+import { UpdateUserDto } from "src/user/dto/update-user.dto";
 
-export type SchemaType = CreateUserDto | LoginUserDto;
+export type SchemaType = CreateUserDto | LoginUserDto | UpdateUserDto;
