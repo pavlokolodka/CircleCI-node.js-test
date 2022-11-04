f25fa4f46e7ffc340702c3965bf4380e8648b6aa
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-11-04 17:07:35 +0200

    Add validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AjvValidationPipe } from 'src/utils/validator/validation';
+import { CreateUserSchema, LoginUserSchema } from 'src/utils/validator/schemes';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/sign-up')
+  @UsePipes(new AjvValidationPipe(CreateUserSchema))
   async register(@Body() user: CreateUserDto, @Res() res) {
     const token = await this.authService.register(user);
     res.set('Authorization', `Bearer ${token}`);
@@ -15,6 +18,7 @@ export class AuthController {
   }
 
   @Post('/sign-in')
+  @UsePipes(new AjvValidationPipe(LoginUserSchema))
   async login(@Body() user: LoginUserDto, @Res() res) {
     const token = await this.authService.login(user);
     res.set('Authorization', `Bearer ${token}`);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const port = process.env.DEV_PORT;
+const port = process.env.DEV_PORT!;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port);
   console.log(`Server is listening on port: ${port}`);
 }
+
 bootstrap();
