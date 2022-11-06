84274d71974b7f2a07b0a56f4ad3700af5288528
TanyaDovzhenko <tanyadovzzhenko@gmail.com>
2022-11-06 20:37:30 +0200

    Revert "add swagger"
    This reverts commit 75f57fce5df2fe689e835feff8b40fffeeb77fdb.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,11 @@ import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
-
-@ApiTags('Authorization')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
-  @ApiResponse({ status: 201, description: 'User created' })
   @Post('/sign-up')
   async register(@Body() user: CreateUserDto, @Res() res) {
     const token = await this.authService.register(user);
@@ -18,7 +14,6 @@ export class AuthController {
     res.send({ message: 'Success' });
   }
 
-  @ApiResponse({ status: 200, description: 'User signed in' })
   @Post('/sign-in')
   async login(@Body() user: LoginUserDto, @Res() res) {
     const token = await this.authService.login(user);
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,18 +1,5 @@
-import { ApiProperty } from "@nestjs/swagger"
-
 export class UpdateUserDto {
-    @ApiProperty({
-        example: 1, description: 'User id', type: Number
-    })
     userId: number
-
-    @ApiProperty({
-        example: 'Name', description: 'User name', type: String
-    })
     name: string
-
-    @ApiProperty({
-        example: 'Lastname', description: 'User lastname', type: String
-    })
     lastname: string
 }
