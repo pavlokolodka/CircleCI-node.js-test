5069c3335973e0124772c1fe962315a11ee3a624
TanyaDovzhenko <tanyadovzzhenko@gmail.com>
2022-11-06 20:40:26 +0200

    Revert "Revert "add swagger""
    This reverts commit 84274d71974b7f2a07b0a56f4ad3700af5288528.

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,7 +1,40 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'email@gmail.com',
+    description: 'User email',
+    uniqueItems: true,
+    type: String
+  })
   email: string;
+
+  @ApiProperty({
+    example: 'Name',
+    description: 'User name',
+    type: String
+  })
   name: string;
+
+  @ApiProperty({
+    example: 'Lastname',
+    description: 'User lastname',
+    type: String
+  })
   lastname: string;
+
+  @ApiProperty({
+    example: 'qwerty123',
+    description: 'User password',
+    type: String
+  })
   password: string;
+
+  @ApiProperty({
+    example: 'https://www.google.com/photo.png',
+    description: 'User photo',
+    required: false,
+    type: String
+  })
   photo?: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Patch } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
+  @ApiResponse({ status: 200, description: 'Return updated user' })
   @Patch()
   updateUser(@Body() updateUserDto: UpdateUserDto) {
     return this.userService.updateUser(updateUserDto)
