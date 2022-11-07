c39048436ada3e702aec5074fb4572f33b44f9f0
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-11-07 11:13:56 +0200

    Change restructure file location

--- src/utils/validator/schemes.ts
+++
@@ -1,3 +0,0 @@
-export { CreateUserSchema } from './user/createUser.schema';
-export { LoginUserSchema } from './user/loginUser.schema';
-export { UpdateUserSchema } from './user/updateUser.schema';
