fcdb9a676d58405bc60bfe020ca7ef72f7addaae
Taras Mysiv <tarasmysiv@gmail.com>
2022-11-07 12:12:49 +0200

    fix remarks

--- src/volunteer/volunteer.service.ts
+++ src/volunteer/volunteer.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../services/prisma.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
@@ -9,18 +9,22 @@ export class VolunteerService {
 
   async getAllOrders(limit: number, sort, page: number, search: string) {
     const skip = limit * (page - 1);
-    const orders = await this.prismaService.order.findMany({
-      skip,
-      take: limit,
-      orderBy: {
-        id: sort,
-      },
-      where: {
-        title: {
-          contains: search,
+    const orders = await this.prismaService.order
+      .findMany({
+        skip,
+        take: limit,
+        orderBy: {
+          id: sort,
         },
-      },
-    });
+        where: {
+          title: {
+            contains: search,
+          },
+        },
+      })
+      .catch(() => {
+        throw new BadRequestException('Something went wrong');
+      });
     const totalPages = Math.round(
       (await this.prismaService.order.findMany()).length / limit,
     );
@@ -33,30 +37,42 @@ export class VolunteerService {
   }
 
   async getOrderById(id: number) {
-    return this.prismaService.order.findFirst({
-      where: {
-        id,
-      },
-    });
+    return this.prismaService.order
+      .findFirst({
+        where: {
+          id,
+        },
+      })
+      .catch(() => {
+        throw new BadRequestException('Order not found');
+      });
   }
 
   async createOrder(order: CreateOrderDto) {
-    return this.prismaService.order.create({
-      data: {
-        title: order.title,
-        info: order.info,
-        user_id: order.id,
-      },
-    });
+    return this.prismaService.order
+      .create({
+        data: {
+          title: order.title,
+          info: order.info,
+          user_id: order.id,
+        },
+      })
+      .catch(() => {
+        throw new BadRequestException('Order not created');
+      });
   }
 
   async updateOrder(order: UpdateOrderDto, id: number) {
-    return this.prismaService.order.update({
-      where: { id },
-      data: {
-        title: order.title,
-        info: order.info,
-      },
-    });
+    return this.prismaService.order
+      .update({
+        where: { id },
+        data: {
+          title: order.title,
+          info: order.info,
+        },
+      })
+      .catch(() => {
+        throw new BadRequestException('Order not updated');
+      });
   }
 }
