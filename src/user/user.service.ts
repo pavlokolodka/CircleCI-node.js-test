ba5fd13efd96395c2811f44d89eb7bd2a1ccfd7d
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-11-09 13:24:33 +0200

    Add user and order repositories, remove user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,25 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/services/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/auth/dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import UserRepository from './repository/user.repository';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private userRepository: UserRepository) {}
+  async getByEmail(email: string) {
+    const user = await this.userRepository.getByEmail(email);
+    return user;
+  }
+
+  async delete(email: string) {
+    const user = await this.userRepository.delete(email);
+    return user;
+  }
+
+  async create(user: CreateUserDto) {
+    const newUser = await this.userRepository.create(user);
+    return newUser;
+  }
 
   async get(email: string) {
     const user = await this.prisma.user.findUnique({
@@ -16,16 +31,7 @@ export class UserService {
   }
 
   async updateUser({ userId, name, lastname }: UpdateUserDto) {
-    return await this.prisma.user
-      .update({
-        where: { id: userId },
-        data: {
-          name: name != null ? name : undefined,
-          lastname: lastname != null ? lastname : undefined,
-        },
-      })
-      .catch(() => {
-        throw new BadRequestException('Something went wrong.');
-      });
+    const user = await this.userRepository.update({ userId, name, lastname });
+    return user;
   }
 }
