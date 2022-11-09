f378c5ac69b8ee82ac1d55165fd6513649bfde9b
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-11-09 17:20:05 +0200

    Add repository query method

--- src/repository/repository.ts
+++ src/repository/repository.ts
@@ -1,5 +1,14 @@
+import { Prisma } from "@prisma/client";
 import { PrismaService } from "src/services/prisma.service";
 
 export default abstract class Repository {
   constructor(protected readonly prismaService: PrismaService) {}
+
+  async query(query: string) {
+    const data = await this.prismaService.$queryRaw(
+      Prisma.sql`${query}`
+    );
+
+    return data;
+  }
 }
