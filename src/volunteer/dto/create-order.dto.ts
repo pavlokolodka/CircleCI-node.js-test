e23fcc5746ff7db6d05f48037c58d0b80e8f7d3b
TanyaDovzhenko <tanyadovzzhenko@gmail.com>
2022-11-14 17:45:11 +0200

    add values to orders route

--- src/volunteer/dto/create-order.dto.ts
+++ src/volunteer/dto/create-order.dto.ts
@@ -1,5 +1,9 @@
 export class CreateOrderDto {
   title: string;
   info: string;
-  id: number;
+  user_id: number;
+  photo: string;
+  goal_amount: number
+  short_info: string;
+  finished_at: Date;
 }
