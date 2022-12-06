cf99339a32cab8b452280918e13c54c1cfa53a84
Taras Mysiv <tarasmysiv@gmail.com>
2022-12-06 15:03:42 +0200

    update transactions

--- src/hint/hint.service.ts
+++ src/hint/hint.service.ts
@@ -3,7 +3,6 @@ import HintRepository from './repository/hint.repository';
 import { UserService } from '../user/user.service';
 import { CreateHintDto } from './dto/create-hint.dto';
 import { UpdateHintDto } from './dto/update-hint.dto';
-import { AwsBucketFolders } from '../types';
 import { AwsService } from '../services';
 
 @Injectable()
@@ -22,10 +21,6 @@ export class HintService {
     return this.hintRepository.getHintById(id);
   }
 
-  async getHintPhotoById(id: number) {
-    return this.hintRepository.getHintPhotoById(id);
-  }
-
   async getAllPhotosByHintId(hintId: number) {
     return this.hintRepository.getAllPhotosByHintId(hintId);
   }
@@ -33,48 +28,12 @@ export class HintService {
   async createHint(hint: CreateHintDto, email: string) {
     const userFromDb = await this.userService.getByEmail(email);
     if (!userFromDb) throw new BadRequestException('Something went wrong');
-    const newHint = await this.hintRepository.createHint(hint, userFromDb.id);
-    if (hint.photos.length && newHint) {
-      await this.createHintPhoto(hint.photos, newHint.id);
-    }
-    return newHint;
-  }
-
-  async createHintPhoto(photos: string[], hintId: number) {
-    photos.map(async (photo) => {
-      photo = await this.awsService.uploadImg(
-        photo,
-        AwsBucketFolders.HINTPHOTO,
-      );
-      return this.hintRepository.createHintPhoto(photo, hintId);
-    });
+    return this.hintRepository.createHint(hint, userFromDb.id);
   }
 
   async updateHintById(id: number, hint: UpdateHintDto) {
     const hintFromDb = await this.getHintById(id);
     if (!hintFromDb) throw new BadRequestException('Hint not found.');
-    const updateHint = await this.hintRepository.updateHintById(id, hint);
-    if (updateHint) {
-      await this.updateHintPhotoById(hintFromDb.id, hint.photos);
-    }
-    return updateHint;
-  }
-
-  async updateHintPhotoById(hintId: number, photos: string[]) {
-    if (photos.length && hintId) {
-      const photosByHintId = await this.getAllPhotosByHintId(hintId);
-
-      if (photosByHintId.length) {
-        photosByHintId.map(async (item) => {
-          await this.awsService.deleteFile(item.photo);
-          await this.deletePhotoById(item.id);
-        });
-      }
-    }
-    return this.createHintPhoto(photos, hintId);
-  }
-
-  async deletePhotoById(id: number) {
-    return this.hintRepository.deletePhotoById(id);
+    return this.hintRepository.updateHintById(id, hint);
   }
 }
--- src/hint/repository/hint.repository.ts
+++ src/hint/repository/hint.repository.ts
@@ -1,8 +1,14 @@
-import Repository from '../../repository/repository';
 import { BadRequestException } from '@nestjs/common';
 import { ICreateHint, IQueryParams, IUpdateHint } from '../interfaces';
+import { AwsService, PrismaService } from '../../services';
+import { AwsBucketFolders } from '../../types';
 
-export default class HintRepository extends Repository {
+export default class HintRepository {
+  constructor(
+    private awsService: AwsService,
+    private prismaService: PrismaService,
+  ) {}
+
   async getAllHints(params: IQueryParams) {
     const { limit, page, search, sort } = params;
     const skip = limit * (page - 1);
@@ -48,53 +54,36 @@ export default class HintRepository extends Repository {
       });
   }
 
-  async createHint(hint: ICreateHint, user_id: number) {
-    return this.prismaService.volunteer_hint
-      .create({
-        data: {
-          info: hint.info,
-          title: hint.title,
-          user_id,
-        },
-      })
-      .catch(() => {
-        throw new BadRequestException('Something went wrong');
-      });
-  }
-
-  async updateHintById(id: number, hint: IUpdateHint) {
-    return this.prismaService.volunteer_hint
-      .update({
-        where: {
-          id,
-        },
-        data: {
-          title: hint.title != null ? hint.title : undefined,
-          info: hint.info != null ? hint.info : undefined,
-        },
-      })
-      .catch(() => {
-        throw new BadRequestException('Something went wrong');
-      });
-  }
-
-  async getHintPhotoById(id: number) {
-    return this.prismaService.volunteer_hint_photo
-      .findFirst({
-        where: {
-          id,
-        },
-      })
-      .catch(() => {
-        throw new BadRequestException('Something went wrong');
-      });
-  }
-
   async getAllPhotosByHintId(hint_id: number) {
     return this.prismaService.volunteer_hint_photo.findMany({
       where: {
         hint_id,
       },
+    });
+  }
+
+  async createHint(hint: ICreateHint, user_id: number) {
+    await this.prismaService.$transaction(async (tx) => {
+      const newHint = await tx.volunteer_hint
+        .create({
+          data: {
+            info: hint.info,
+            title: hint.title,
+            user_id,
+          },
+        })
+        .catch(() => {
+          throw new BadRequestException('Something went wrong');
+        });
+      hint.photos.map(async (photo) => {
+        photo = await this.awsService
+          .uploadImg(photo, AwsBucketFolders.HINTPHOTO)
+          .catch(() => {
+            throw new BadRequestException('Something went wrong');
+          });
+        return this.createHintPhoto(photo, newHint.id);
+      });
+      return newHint;
     });
   }
 
@@ -109,6 +98,41 @@ export default class HintRepository extends Repository {
       .catch(() => {
         throw new BadRequestException('Something went wrong');
       });
+  }
+
+  async updateHintById(id: number, hint: IUpdateHint) {
+    await this.prismaService.$transaction(async (tx) => {
+      await tx.volunteer_hint
+        .update({
+          where: {
+            id,
+          },
+          data: {
+            title: hint.title != null ? hint.title : undefined,
+            info: hint.info != null ? hint.info : undefined,
+          },
+        })
+        .catch(() => {
+          throw new BadRequestException('Something went wrong');
+        });
+      await this.updateHintPhotoById(id, hint.photos);
+    });
+  }
+
+  async updateHintPhotoById(hintId: number, photos: string[]) {
+    if (photos.length && hintId) {
+      const photosByHintId = await this.getAllPhotosByHintId(hintId);
+
+      if (photosByHintId.length) {
+        photosByHintId.map(async (item) => {
+          await this.awsService.deleteFile(item.photo);
+          await this.deletePhotoById(item.id);
+        });
+      }
+      photos.map((photo) => {
+        return this.createHintPhoto(photo, hintId);
+      });
+    }
   }
 
   async deletePhotoById(id: number) {
