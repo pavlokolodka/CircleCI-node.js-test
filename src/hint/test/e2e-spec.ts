b650bbf590621c0a50186e35b0580398b9f3f9b1
Taras Mysiv <tarasmysiv@gmail.com>
2022-12-16 14:28:16 +0200

    add tests for creating hints

--- src/hint/test/e2e-spec.ts
+++ src/hint/test/e2e-spec.ts
@@ -5,10 +5,43 @@ import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
 import { hintMock } from './hint.mock';
 import { RolesGuard } from '../../auth/guards/roles.guard';
+import { AuthHandleService, PrismaService } from '../../services';
+import { userMock } from '../../user/repository/user.repository.mock';
 
 describe('Hint', () => {
   let app: INestApplication;
   let hintService: HintService;
+  const prismaService = new PrismaService();
+
+  beforeAll(async () => {
+    await prismaService.user
+      .create({
+        data: {
+          email: userMock().email,
+          name: userMock().name,
+          lastname: userMock().lastname,
+          role: userMock().role,
+        },
+      })
+      .catch(() => {
+        return;
+      });
+    await prismaService.volunteer_hint
+      .create({
+        data: {
+          title: hintMock().title,
+          info: hintMock().info,
+          user_id: userMock().id,
+        },
+      })
+      .catch(() => {
+        return;
+      });
+  });
+
+  const mockAuthHandleService = {
+    getPayload: jest.fn().mockImplementation(() => userMock()),
+  };
 
   const mockGuard = jest.fn().mockImplementation(() => true);
 
@@ -18,6 +51,8 @@ describe('Hint', () => {
     })
       .overrideGuard(RolesGuard)
       .useValue(mockGuard)
+      .overrideProvider(AuthHandleService)
+      .useValue(mockAuthHandleService)
       .compile();
 
     hintService = moduleRef.get<HintService>(HintService);
@@ -89,6 +124,9 @@ describe('Hint', () => {
         .send(hint);
       expect(response.status).toBe(201);
       expect(response.body.title).toEqual(hint.title);
+      await prismaService.volunteer_hint.delete({
+        where: { id: response.body.id },
+      });
     });
   });
 
@@ -106,6 +144,8 @@ describe('Hint', () => {
   });
 
   afterAll(async () => {
+    await prismaService.volunteer_hint.delete({ where: { id: hintMock().id } });
+    await prismaService.user.delete({ where: { email: userMock().email } });
     await app.close();
   });
 });
